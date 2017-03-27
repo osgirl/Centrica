829b56a6bc91489e0dd923b6336f348b4a01f7ce
sajeevjos <sajeevjos@gmail.com>
2017-03-27 18:35:34 +0100

    Removing unwanted files

--- CentricaTasks1/PaymentCardConsumer/RabbitMQ/CardPayment.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PaymentCardConsumer.RabbitMQ
-{
-    public class CardPayment
-    {
-        public decimal Amount { get; set; }
-        public string CardNumber { get; set; }
-        public string Name { get; set; }
-    }
-}
